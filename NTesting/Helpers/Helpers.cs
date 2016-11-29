5b23c89bb2957000ace22a1862a895ed5393455d
klausbachmann <martin@derwolters.de>
2016-11-29 11:12:23 +0100

    Helper Klasse

---
+++ NTesting/Helpers/Helpers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTesting.Helpers
+{
+    static class Helpers
+    {
+        public static string getDayName(DayOfWeek dayOfWeek)
+        {
+            switch (dayOfWeek)
+            {
+                case DayOfWeek.Monday:
+                    return "Mo";
+                case DayOfWeek.Tuesday:
+                    return "Di";
+                case DayOfWeek.Wednesday:
+                    return "Mi";
+                case DayOfWeek.Thursday:
+                    return "Do";
+                case DayOfWeek.Friday:
+                    return "Fr";
+                case DayOfWeek.Saturday:
+                    return "Sa";
+                case DayOfWeek.Sunday:
+                    return "So";
+
+                default: return "Mo";
+            }
+        }
+    }
+}
